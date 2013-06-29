a6e43ff8436f651f6d9ef92fdf48ce747510f7bc
Remi Papillie <remi.papillie@gmail.com>
2013-06-29 13:15:03 +0200

    New dimensions shader

--- shaders/dimensions.glsl
+++ shaders/dimensions.glsl
@@ -12,28 +12,29 @@ void main(void)
 }
 
 //! FRAGMENT
+
 const float infinity = 10000.0;
 const vec3 skyColor = vec3(0.7, 0.9, 1.0);
 const float fogDensity = 0.004;
 const vec3 lightColor = vec3(0.5, 1.0, 0.9);
+float pulse;
 
 float spheres(vec3 pos, float r)
 {
   return length(mod(pos, 30.0) - vec3(15.0)) - r;
 }
 
-
 float map(vec3 pos)
 {
-  return spheres(pos, ((1. + cos(time * 2.)) * 1.5));
+  return spheres(pos, ((2. + pulse) * 1.5));
 }
 
 vec3 traceRay(vec3 pos, vec3 dir)
 {
-   for (int i = 0; i < 24; i++)
+   for (int i = 0; i < 30; i++)
    {
        float distance = map(pos);
-       pos += 0.9 * distance * dir;
+       pos += distance * dir;
    }
   
    return pos;
@@ -53,20 +54,21 @@ vec3 calcNormal(vec3 pos)
 void main(void)
 {
   vec2 uv = gl_FragCoord.xy / res.xy;
+	pulse = exp(-fract(time * 2.));
   
   vec3 pos = vec3(time * 50., sin(time) * 20., 0.);
   vec3 dir = normalize(vec3(
     (gl_FragCoord.x - res.x * 0.5) / res.y, gl_FragCoord.y / res.y - 0.5,
-    cos(time) * 1.5));
+    sin(time * 1.57) * 0.4 + 1.2));
   
-  vec3 color = vec3(.9, 0., 0.);
+  vec3 color = vec3(.9, .4, 0.0);
     vec3 point = traceRay(pos, dir);
     vec3 normal = calcNormal(point);
   
-  float fogFactor = 1.0 - (1.0 / exp(point.z * 0.0025));
-    float diffuse = max(dot(normal, vec3(.9, .9, .9)), 0.0);
+  float fogFactor = 1.0 - (1.0 / exp(abs(point.z) * 0.01));
+    float diffuse = max(dot(normal, normalize(vec3(1.0))), 0.0) * 2.0 + pulse * 0.4;
   
-    color = mix(color * diffuse, vec3(1.), fogFactor);
+    color = mix(color * diffuse, vec3(1., 0.95, 0.8), fogFactor);
   
   gl_FragColor = vec4(color,1.0);
 }
